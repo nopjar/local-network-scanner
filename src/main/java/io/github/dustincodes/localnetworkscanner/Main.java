903ea812b06b4964b747d8393ebe0ea7a7b7097b
dustin-codes <dustin.codes02@gmail.com>
2021-08-22 12:57:42 +0200

    clean up code and add javadoc

--- src/main/java/io/github/dustincodes/localnetworkscanner/Main.java
+++ src/main/java/io/github/dustincodes/localnetworkscanner/Main.java
@@ -10,26 +10,32 @@ public class Main {
     private static final byte SEARCHED_SUBNET = 21;
     private static final long DEVICE_LOOKUP_PERIOD = 5000;
 
+    private static NetworkScanner scanner;
+
     public static void main(String[] args) {
-        System.out.println("[" + currentTime() + "] Starting networkscanner...");
+        log("Starting networkscanner...");
         Runtime.getRuntime().addShutdownHook(new Thread(Main::shutdown));
-        NetworkScanner scanner = new NetworkScanner();
+        scanner = new NetworkScanner();
 
-        System.out.println("[" + currentTime() + "] Scanning for reachable subnets in network...");
+        log("Scanning for reachable subnets in network...");
         List<InetAddress> foundInetAddresses = scanner.scanLocalNetwork();
-        System.out.println("[" + currentTime() + "] Addresses found:  " + foundInetAddresses.size());
+        log("Addresses found:  " + foundInetAddresses.size());
 
-        System.out.println("[" + currentTime() + "] Starting timerthread for device lookup...");
+        log("Starting timerthread for device lookup...");
+        startLookupTimer();
+    }
+
+    private static void startLookupTimer() {
         Timer timer = new Timer(false);
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("[" + currentTime() + "] Searching for subnet " + SEARCHED_SUBNET + "...");
+                log("Searching for subnet " + SEARCHED_SUBNET + "...");
                 InetAddress mobilePhone = scanner.getInetAddressInLocalNetwork(SEARCHED_SUBNET);
                 if (mobilePhone == null) {
-                    System.out.println("[" + currentTime() + "] Device is not in local network.");
+                    log("Device is not in local network.");
                 } else {
-                    System.out.println("[" + currentTime() + "] Found " + mobilePhone.getHostName());
+                    log("Found " + mobilePhone.getHostName());
                 }
             }
         }, 0L, DEVICE_LOOKUP_PERIOD);
@@ -40,7 +46,11 @@ public class Main {
     }
 
     private static void shutdown() {
-        System.out.println("[" + currentTime() + "] Exiting networkscanner...");
+        log("Exiting networkscanner...");
+    }
+
+    private static void log(String message) {
+        System.out.println("[" + currentTime() + "] " + message);
     }
 
 }
--- src/main/java/io/github/dustincodes/localnetworkscanner/NetworkScanner.java
+++ src/main/java/io/github/dustincodes/localnetworkscanner/NetworkScanner.java
@@ -5,11 +5,25 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * A simple class which returns InetAddresses connected to your local network.
+ */
 public class NetworkScanner {
 
     public NetworkScanner() {
     }
 
+    /**
+     * Scans the local network for all subnets (1 - 254).
+     * <p>
+     * This method will return any {@link InetAddress} which returns true with {@link InetAddress#isReachable(int)} by
+     * a timeout from 5000 milliseconds.
+     * <p>
+     * {@link SocketException}s will be ignored as I have no idea when they are thrown and what they mean. If you know
+     * it, let me know please.
+     *
+     * @return a list of reachable InetAddresses
+     */
     public List<InetAddress> scanLocalNetwork() {
         final List<InetAddress> reachableAddresses = new ArrayList<>();
         final byte[] ip = getLocalIp();
@@ -46,6 +60,17 @@ public class NetworkScanner {
         return reachableAddresses;
     }
 
+    /**
+     * Tries to find the IP-Address with the specified subnet in your local network.
+     * <p>
+     * The subnet is for example: {@code xxx.xxx.xxx.21}
+     * <p>
+     * If you want to find a specific device (like your phone) over the ip-address, make sure your router always assigns
+     * the same ip-address to your device. You can set this up in the interface of your router.
+     *
+     * @param subnet the subnet of the device to search for
+     * @return the corresponding InetAddress of the subnet, may be null if ip-address is not reachable
+     */
     public InetAddress getInetAddressInLocalNetwork(byte subnet) {
         final byte[] ip = getLocalIp();
         if (ip == null) {
