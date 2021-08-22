a1adc84c028a33a3d43ab1c8e582a93b7593e4b3
dustin-codes <dustin.codes02@gmail.com>
2021-08-22 14:38:25 +0200

    add /docs

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.dustincodes.localnetworkscanner","c":"NetworkScanner","l":"getInetAddressInLocalNetwork(byte)"},{"p":"io.github.dustincodes.localnetworkscanner","c":"NetworkScanner","l":"NetworkScanner()","u":"%3Cinit%3E()"},{"p":"io.github.dustincodes.localnetworkscanner","c":"NetworkScanner","l":"scanLocalNetwork()"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"io.github.dustincodes.localnetworkscanner","l":"NetworkScanner"}];updateSearchResults();
